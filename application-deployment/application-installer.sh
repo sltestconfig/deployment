f291a9d45ee9919bb756053ce1e91e04fb27234c
skhalid <skhalid@verizonmedia.com>
2021-11-06 08:52:01 -0500

    Building Server deployment & management tool

--- application-deployment/application-installer.sh
+++ application-deployment/application-installer.sh
@@ -18,8 +18,6 @@ if [ $DEPLOYEDTAG != $NEWAPPTAG ];then
 
 cp -v $HOME/application-deployment/99-application-info /etc/update-motd.d/99-application-info
 
-
-
 ###### INSTALL APACHE2 ######
 
 apt update
@@ -33,7 +31,5 @@ systemctl restart apache2
 apt install -y php libapache2-mod-php
 cp $HOME/application-deployment/index.php /var/www/html/index.php
 cp $HOME/application-deployment/info.php /var/www/html/info.php
-
-
 
 fi
