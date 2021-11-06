93d2402ed3e3a93c2cccc4e457ea28a2990229b8
skhalid <skhalid@verizonmedia.com>
2021-11-05 19:37:18 -0500

    Building Server deployment & management tool

--- application-deployment/application-installer.sh
+++ application-deployment/application-installer.sh
@@ -15,3 +15,6 @@ systemctl restart apache2
 apt install -y php libapache2-mod-php
 cp $HOME/application-deployment/index.php /var/www/html/index.php
 cp $HOME/application-deployment/info.php /var/www/html/info.php
+
+###### ###### 
+###### ###### 
