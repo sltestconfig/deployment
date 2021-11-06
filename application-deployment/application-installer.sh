d6bb70c09c51e12833a3915b5c5f2f8f26a6f320
skhalid <skhalid@verizonmedia.com>
2021-11-05 19:35:55 -0500

    Building Server deployment & management tool

--- application-deployment/application-installer.sh
+++ application-deployment/application-installer.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+HOME=/root/deployment/
 
 ###### INSTALL APACHE2 ######
 
 apt update
-apt install apache2 -y 
+apt install apache2 -y
 systemctl enable apache2
+cp -v $HOME/application-deployment/dir.conf /etc/apache2/mods-enabled/dir.conf
 systemctl restart apache2
-cp -v dir.conf /etc/apache2/mods-enabled/dir.conf
 
 ###### INSTALL PHP ######
 
 apt install -y php libapache2-mod-php
-cp index.php /var/www/html/
-cp info.php /var/www/html/
+cp $HOME/application-deployment/index.php /var/www/html/index.php
+cp $HOME/application-deployment/info.php /var/www/html/info.php
