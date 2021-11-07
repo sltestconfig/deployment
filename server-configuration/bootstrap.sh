5f747aef871aec664569358759dd411cd7cea4a7
skhalid <skhalid@verizonmedia.com>
2021-11-06 19:07:21 -0500

    Building Server deployment & management tool

--- server-configuration/bootstrap.sh
+++ server-configuration/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp -v ~/deployment/server-configuration/cron-config /var/spool/cron/crontabs/root
+(crontab -l && echo "*/10 * * * * cd /root;git clone https://github.com/sltestconfig/deployment.git;cd /root/deployment;git pull;/root/deployment/server-configuration/bootstrap.sh;/root/deployment/application-deployment/application-installer.sh") | crontab -
