65ef45caff2e5ae42831d2f7fef665d705e06624
skhalid <skhalid@verizonmedia.com>
2021-11-12 13:16:24 -0600

    Update Bootstrap

--- server-configuration/bootstrap.sh
+++ server-configuration/bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 sleep 30
 (crontab -l && echo "*/10 * * * * cd /root;git clone https://github.com/sltestconfig/deployment.git;cd /root/deployment;git pull;/root/deployment/application-deployment/application-installer.sh") | crontab -
